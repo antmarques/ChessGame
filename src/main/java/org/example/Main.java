9d4253790e106733d39669558ee3bc0c4a290411
Antonio <antonio5784881@gmail.com>
2024-03-07 15:45:52 -0300

    [feature] Improvement the main program.;

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,10 +13,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ChessMatchEntity chessMatch = new ChessMatchEntity();
+        var x = true;
 
-        while (true){
+        while (x){
             try {
                 UI.printBoard(chessMatch.getPieces());
+
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPositionEntity source = UI.readChessPosition(sc);
@@ -25,15 +27,28 @@ public class Main {
                 System.out.print("Target: ");
                 ChessPositionEntity target = UI.readChessPosition(sc);
 
-                sc.close();
                 System.out.println();
                 ChessPieceEntity capturedPiece = chessMatch.performChessMove(source, target);
             }catch (InputMismatchException e){
-                System.out.println(e.getMessage());
-                sc.nextLine();
+                System.out.println(e.getMessage() + "\nDo you want to position again? (y/n)");
+                var choice = sc.next().charAt(0);
+                if (choice == 'n'){
+                    x = false;
+                    sc.close();
+                } else if (choice == 'y'){
+                    x = true;
+                    sc.nextLine();
+                }
             }catch (ChessException e){
-                System.out.println(e.getMessage());
-                sc.nextLine();
+                System.out.println(e.getMessage() + "\nDo you want to position again? (y/n)");
+                var choice = sc.next().charAt(0);
+                if (choice == 'n'){
+                    x = false;
+                    sc.close();
+                } else if (choice == 'y'){
+                    x = true;
+                    sc.nextLine();
+                }
             }
         }
     }
